d966b17b150485b7d38b44eb4b0ee4bd7d8654e7
Will Page <compenguy@gmail.com>
2020-08-02 22:11:22 -0700

    Add macro support for board pin definitions, minor gpio cleanups

--- hal/src/gpio.rs
+++ hal/src/gpio.rs
@@ -2,7 +2,12 @@
 use crate::hal::digital::{InputPin, OutputPin, StatefulOutputPin, ToggleableOutputPin};
 
 // SAM3A4C, SAM3A8C, SAM3X4C, and SAM3X8C (100-pin) only have PIOA-PIOB
-#[cfg(any(feature = "atsam3a4c", feature = "atsam3a8c", feature = "atsam3x4c", feature = "atsam3x8c"))]
+#[cfg(any(
+    feature = "atsam3a4c",
+    feature = "atsam3a8c",
+    feature = "atsam3x4c",
+    feature = "atsam3x8c"
+))]
 use crate::target_device::{PIOA, PIOB};
 
 // SAM3X4E and SAM3X8E (144-pin) have PIOA-PIOD
@@ -14,7 +19,6 @@ use crate::target_device::{PIOA, PIOB, PIOC, PIOD};
 use crate::target_device::{PIOA, PIOB, PIOC, PIOD, PIOE, PIOF};
 
 use core::marker::PhantomData;
-use paste::paste;
 
 /// PIO controller configuration register block.
 pub struct PioGroup<PIOn> {
@@ -76,14 +80,14 @@ macro_rules! pin {
         $pin_ident:ident,
         $pin_no:expr
     ) => {
-        paste! {
+        crate::paste! {
         /// Represents the IO pin with the matching name.
         pub struct $PinType<MODE> {
             _mode: PhantomData<MODE>,
         }
         } // end paste
 
-        paste! {
+        crate::paste! {
         /// Represents an unconfigured IO pin.
         pub type [<Unconfigured $PinType>] = $PinType<Unconfigured>;
         impl Default for [<Unconfigured $PinType>] {
@@ -105,7 +109,7 @@ macro_rules! pin {
         impl<MODE> $PinType<MODE> {
             /// Configures the pin to operate as a floating input
             pub fn into_floating_input(self) -> $PinType<Input<Floating>> {
-                paste! {
+                crate::paste! {
                 // Enable PIO (not peripheral) mode
                 unsafe {(*$group::ptr()).per.write_with_zero(|w| w.[<p $pin_no>]().set_bit());}
                 // Disable output mode (input)
@@ -118,7 +122,7 @@ macro_rules! pin {
 
             /// Configures the pin to operate as a pulled-up input
             pub fn into_pull_up_input(self) -> $PinType<Input<PullUp>> {
-                paste! {
+                crate::paste! {
                 // Enable PIO (not peripheral) mode
                 unsafe {(*$group::ptr()).per.write_with_zero(|w| w.[<p $pin_no>]().set_bit());}
                 // Disable output mode (input)
@@ -131,7 +135,7 @@ macro_rules! pin {
 
             /// Configures the pin to operate as an open-drain output
             pub fn into_open_drain_output(self) -> $PinType<Output<OpenDrain>> {
-                paste! {
+                crate::paste! {
                 // Enable PIO (not peripheral) mode
                 unsafe {(*$group::ptr()).per.write_with_zero(|w| w.[<p $pin_no>]().set_bit());}
                 // Enable output mode
@@ -144,7 +148,7 @@ macro_rules! pin {
 
             /// Configures the pin to operate as an push-pull output
             pub fn into_push_pull_output(self) -> $PinType<Output<PushPull>> {
-                paste! {
+                crate::paste! {
                 // Enable PIO (not peripheral) mode
                 unsafe {(*$group::ptr()).per.write_with_zero(|w| w.[<p $pin_no>]().set_bit());}
                 // Enable output mode
@@ -159,7 +163,7 @@ macro_rules! pin {
 
             /// Configures the pin to function as the primary (A) attached peripheral.
             pub fn into_peripheral_a(self) -> $PinType<PfA> {
-                paste! {
+                crate::paste! {
                 // Disable PIO (enable peripheral) mode
                 unsafe {(*$group::ptr()).pdr.write_with_zero(|w| w.[<p $pin_no>]().set_bit());}
                 // Select Peripheral A (0 -> A, 1 -> B)
@@ -170,7 +174,7 @@ macro_rules! pin {
 
             /// Configures the pin to function as the alternate (B) attached peripheral.
             pub fn into_peripheral_b(self) -> $PinType<PfB> {
-                paste! {
+                crate::paste! {
                 // Disable PIO (enable peripheral) mode
                 unsafe {(*$group::ptr()).pdr.write_with_zero(|w| w.[<p $pin_no>]().set_bit());}
                 // Select Peripheral B (0 -> A, 1 -> B)
@@ -201,7 +205,7 @@ macro_rules! pin {
             }
 
             fn set_high_impl(&mut self) {
-                paste! {
+                crate::paste! {
                 unsafe {(*$group::ptr()).sodr.write_with_zero(|w| w.[<p $pin_no>]().bit(true));}
                 } // end paste
             }
@@ -212,7 +216,7 @@ macro_rules! pin {
             }
 
             fn set_low_impl(&mut self) {
-                paste! {
+                crate::paste! {
                 unsafe {(*$group::ptr()).codr.write_with_zero(|w| w.[<p $pin_no>]().bit(true));}
                 } // end paste
             }
@@ -256,7 +260,7 @@ macro_rules! pin {
             /// If $group is not currently clocked, this will return the value from
             /// when it was last clocked.
             pub fn is_high(&self) -> bool {
-                paste! {
+                crate::paste! {
                 unsafe {(*$group::ptr()).pdsr.read().[<p $pin_no>]().bits()}
                 } // end paste
             }
@@ -286,13 +290,20 @@ macro_rules! pin {
 macro_rules! pio_group {
     (
         $group_id:ident,
+        $peripheral_id:expr,
         [
           $($pin_no:expr,)+
         ]
     ) => {
 
-paste! {
+crate::paste! {
 impl PioGroup<[<PIO $group_id:upper>]> {
+    /// Return the Peripheral ID for the PIOF Controller, used for controlling
+    /// clocks and interrupts.
+    pub fn peripheral_id() -> $crate::clock::PeripheralID {
+        $crate::clock::PeripheralID::[<Id $peripheral_id Pio $group_id:upper>]
+    }
+
     /// Instantiate a representation of a PIO group, providing an interface
     /// to all the pins it controls.
     pub fn new(group: [<PIO $group_id:upper>]) -> Self {
@@ -300,7 +311,6 @@ impl PioGroup<[<PIO $group_id:upper>]> {
             group,
         }
     }
-
 
     $(
     /// Access the configuration and state of the pin of this name on this PIO
@@ -328,6 +338,7 @@ $(
 // PIOA has the same pin set among all targets
 pio_group!(
     a,
+    11,
     [
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
         25, 26, 27, 28, 29, 30, 31,
@@ -337,6 +348,7 @@ pio_group!(
 // PIOB has the same pin set among all targets
 pio_group!(
     b,
+    12,
     [
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
         25, 26, 27, 28, 29, 30, 31,
@@ -347,6 +359,7 @@ pio_group!(
 #[cfg(any(feature = "atsam3x4e", feature = "atsam3x8e", feature = "atsam3x8h"))]
 pio_group!(
     c,
+    13,
     [
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
         25, 26, 27, 28, 29, 30,
@@ -356,12 +369,13 @@ pio_group!(
 // PIOD is not supported by the atsam3x?c targets, and only has pins 0-10 on
 // the atsam3x?e targets
 #[cfg(any(feature = "atsam3x4e", feature = "atsam3x8e"))]
-pio_group!(d, [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ]);
+pio_group!(d, 14, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]);
 
 // PIOD has pins 0-30 on the atsam3x8h target
 #[cfg(feature = "atsam3x8h")]
 pio_group!(
     d,
+    14,
     [
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
         25, 26, 27, 28, 29, 30,
@@ -372,6 +386,7 @@ pio_group!(
 #[cfg(feature = "atsam3x8h")]
 pio_group!(
     e,
+    15,
     [
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
         25, 26, 27, 28, 29, 30, 31,
@@ -380,9 +395,102 @@ pio_group!(
 
 // PIOF has pins 0-6 on the atsam3x8h target
 #[cfg(feature = "atsam3x8h")]
-pio_group!(
-    f,
-    [
-        0, 1, 2, 3, 4, 5, 6,
-    ]
-);
+pio_group!(f, 16, [0, 1, 2, 3, 4, 5, 6,]);
+
+/// This macro is a helper for defining a `Pins` type in a board support
+/// crate.  This type is used to provide more meaningful aliases for the
+/// various GPIO pins for a given board.
+#[macro_export]
+macro_rules! define_pins {
+    (
+        $(#[$topattr:meta])* struct $Type:ident,
+        $( $(#[$attr:meta])* pin $name:ident = $pio_group:ident : $pin_ident:expr),+ ,
+    ) => {
+
+$crate::paste! {
+$(#[$topattr])*
+pub struct $Type {
+    /// Opaque reference to PIO controller A
+    pub pa: $crate::gpio::PioGroup<$crate::target_device::PIOA>,
+    /// Opaque reference to PIO controller B
+    pub pb: $crate::gpio::PioGroup<$crate::target_device::PIOB>,
+    /// Opaque reference to PIO controller C
+    #[cfg(any(feature = "sam3_e", feature = "sam3x8h"))]
+    pub pc: $crate::gpio::PioGroup<$crate::target_device::PIOC>,
+    /// Opaque reference to PIO controller D
+    #[cfg(any(feature = "sam3_e", feature = "sam3x8h"))]
+    pub pd: $crate::gpio::PioGroup<$crate::target_device::PIOD>,
+    /// Opaque reference to PIO controller E
+    #[cfg(feature = "sam3x8h")]
+    pub pe: $crate::gpio::PioGroup<$crate::target_device::PIOE>,
+    /// Opaque reference to PIO controller F
+    #[cfg(feature = "sam3x8h")]
+    pub pf: $crate::gpio::PioGroup<$crate::target_device::PIOF>,
+
+    $(
+        $(#[$attr])*
+        pub $name: $crate::gpio::[<P $pio_group $pin_ident>]<$crate::gpio::Unconfigured>
+    ),+
+}
+
+impl $Type {
+    #[cfg(any(feature = "atsam3a4c", feature = "atsam3a8c", feature = "atsam3x4c", feature = "atsam3x8c"))]
+    /// Returns the pins for the device
+    pub fn new(pioa: $crate::target_device::PIOA, piob: $crate::target_device::PIOB) -> Self {
+        let pa = $crate::gpio::PioGroup::from(pioa);
+        let pb = $crate::gpio::PioGroup::from(piob);
+        $(
+        let $name: [<p $pio_group>].[<p $pin_ident>]();
+        )+
+        $Type {
+            pa,
+            pb,
+            $($name),+,
+        }
+    }
+
+    #[cfg(any(feature = "atsam3x4e", feature = "atsam3x8e"))]
+    /// Returns the pins for the device
+    pub fn new(pioa: $crate::target_device::PIOA, piob: $crate::target_device::PIOB, pioc: $crate::target_device::PIOC, piod: $crate::target_device::PIOD) -> Self {
+        let pa = $crate::gpio::PioGroup::from(pioa);
+        let pb = $crate::gpio::PioGroup::from(piob);
+        let pc = $crate::gpio::PioGroup::from(pioc);
+        let pd = $crate::gpio::PioGroup::from(piod);
+        $(
+        let $name = [<p $pio_group>].[<p $pin_ident>]();
+        )+
+        $Type {
+            pa,
+            pb,
+            pc,
+            pd,
+            $($name),+,
+        }
+    }
+
+    #[cfg(feature = "atsam3x8h")]
+    /// Returns the pins for the device
+    pub fn new(pioa: $crate::target_device::PIOA, piob: $crate::target_device::PIOB, pioc: $crate::target_device::PIOC, piod: $crate::target_device::PIOD,  pioe: $crate::target_device::PIOE, piof: $crate::target_device::PIOF) -> Self {
+        let pa = $crate::gpio::PioGroup::from(pioa);
+        let pb = $crate::gpio::PioGroup::from(piob);
+        let pc = $crate::gpio::PioGroup::from(pioc);
+        let pd = $crate::gpio::PioGroup::from(piod);
+        let pe = $crate::gpio::PioGroup::from(pioe);
+        let pf = $crate::gpio::PioGroup::from(piof);
+        $(
+        let $name = [<p $pio_group>].[<p $pin_ident>]();
+        )+
+        $Type {
+            pa,
+            pb,
+            pc,
+            pd,
+            pe,
+            pf,
+            $($name),+,
+        }
+    }
+}
+} // end paste
+
+}}
